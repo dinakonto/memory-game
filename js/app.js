6af9d4dcbc8d73ac639c19bf86aa652c7952d953
Dina Konto <dina.konto@gmail.com>
2020-03-18 09:50:44 +1100

    Added star rating functionality

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
 /*
  *    STILL TO DO
+ *    + star rating that starts with 3-5 and reduces as moves are made
  *    + timer that starts when you show first card, ends when you match all
+ *      + add star rating and time to success modal
  *    + responsive behaviour
  *    + transitions and animations
+ *    + README.md file
+ *    MAYBE
+ *    + keyboard accessibility
  */
 
 
@@ -11,10 +16,6 @@
  GLOBAL VARIABLES
  * * * * * * * * */
 
-// Variable to access cards in the DOM
-const cards = document.querySelectorAll('.card')
-
-// List of card types
 const cardTypes = [
   'fa-cat',
   'fa-crow',
@@ -26,7 +27,10 @@ const cardTypes = [
   'fa-hippo'
 ];
 
-// Target the reset button
+// Get all card elements
+const cards = document.querySelectorAll('.card')
+
+// Get reset button
 const resetButton = document.querySelector('button');
 
 // List of which cards are open
@@ -39,9 +43,12 @@ let moveCount = 0;
  FUNCTIONS
  * * * * * * * * */
 
+//
+// GLOBAL GAME FUNCTIONS
+//
 // Shuffle the cards and display them randomly
 function resetDeck() {
-  // Make two of every card
+  // Make two of every card type
   let deck = [...cardTypes, ...cardTypes]
   // Shuffle function from http://stackoverflow.com/a/2450976
   function shuffle(array) {
@@ -59,40 +66,74 @@ function resetDeck() {
   }
   // Save the shuffled deck
   deck = shuffle(deck);
-
+  //Reset everything global
   openCards = [];
   moveCount = 0;
   let moveDisplay = document.getElementById('move-count');
   moveDisplay.innerText = moveCount;
+  //Reset stars
+  let stars = document.querySelectorAll('.fa-star');
+  for (let star of stars) {
+    star.classList.add('fas');
+    star.classList.remove('far');
+  }
 
   for (let i = 0; i < deck.length; i++ ) {
-    //Reset everything
+    // Reset everything card-specific
     let cards = document.querySelectorAll('.card');
     cards[i].classList.remove('open', 'match', 'nomatch');
-    // Randomly populate the cards
+    // Randomly populate the cards again
     for (let animal in deck) {
       cards[i].innerHTML = `<i class="fas ${deck[i]}"></i>`;
     }
   }
-}
+};
 
+// Keep track of the number of moves
 function moveCounter() {
   moveCount += 1;
+  starRating();
   let moveDisplay = document.getElementById('move-count');
   moveDisplay.innerText = moveCount;
+};
+
+// Star ratings
+function starOff(num) {
+  let stars = document.querySelectorAll('.fa-star');
+  let star = stars.item(num);
+  star.classList.add('far');
+  star.classList.remove('fas');
+}
+function starRating() {
+  if (moveCount === 14) {
+    starOff(4);
+  } else if (moveCount > 17 && moveCount <= 20) {
+    starOff(3);
+  } else if (moveCount > 20 && moveCount <= 24) {
+    starOff(2);
+  } else if (moveCount > 24) {
+    starOff(1);
+  }
 }
 
-// Show the card and add it to the openCards array
+//
+// CARD + MATCHING LOGIC
+//
+// Show card and add it to the openCards array
 function showCard(card) {
-  card.classList.add('open');
-  if (openCards.length < 2) {
-    let iconName = card.querySelector('svg').getAttribute('data-icon');
-    openCards.push(iconName);
-  } if (openCards.length === 2) {
-    // Set move count forward one
-    moveCounter();
-    // Check to see if they match
-    checkMatch();
+  if (card.classList.contains('match') || card.classList.contains('nomatch')) {
+    // Do nothing
+  } else {
+    card.classList.add('open');
+    if (openCards.length < 2) {
+      let iconName = card.querySelector('svg').getAttribute('data-icon');
+      openCards.push(iconName);
+    } if (openCards.length === 2) {
+      // Set move count forward one
+      moveCounter();
+      // Check to see if they match
+      checkMatch();
+    }
   }
 };
 
@@ -100,11 +141,10 @@ function showCard(card) {
 function checkMatch() {
   if (openCards[0] == openCards[1]) {
     hasMatch();
-  }
-  else {
+  } else {
     noMatch();
   }
-}
+};
 
 // If they do match
 function hasMatch() {
@@ -119,7 +159,7 @@ function hasMatch() {
   if (parent.children.length == parent.querySelectorAll('.match').length) {
     completed();
   }
-}
+};
 
 // If they don't match
 function noMatch() {
@@ -131,22 +171,25 @@ function noMatch() {
     choices[1].classList.remove('nomatch', 'open');
   }, 600);
   openCards = [];
-}
+};
 
 // When you've matched everything
 function completed() {
-  // Display success message
+  // Get relevant info
   let modal = document.querySelector('.modal-bg');
-  modal.style.display = 'block';
   let finalMoves = document.querySelector('.moves-final');
-  finalMoves.innerText = moveCount;
-  // Click play again button
+  let finalStars = document.querySelector('.stars').innerHTML;
   let playAgain = document.querySelector('#replay');
+  // Display success message
+  modal.style.display = 'block';
+  finalMoves.innerText = moveCount;
+  document.querySelector('.final-stars').innerHTML = finalStars;
+  // Click play again button
   playAgain.onclick = function() {
     resetDeck();
     modal.style.display = 'none';
   }
-}
+};
 
 /* * * * * * * * *
  EXECUTION
@@ -162,7 +205,7 @@ for (const card of cards) {
       showCard(card);
     };
   });
-}
+};
 
 // Reset the deck and the move counter
 resetButton.onclick = resetDeck;
