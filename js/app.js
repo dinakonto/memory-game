76b55362c2836f34f4f250f83dc01b696ecda6c8
Dina Konto <dina.konto@gmail.com>
2020-03-17 16:10:07 +1100

    Randomly populate the cards on load/refresh

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 /*
- * DECLARE VARIABLES
+ * POPULATE THE HTML
  */
 
-const deckClasses = [
+// List of card types
+const cardTypes = [
   'fa-cat',
   'fa-crow',
   'fa-dog',
@@ -13,36 +14,131 @@ const deckClasses = [
   'fa-hippo'
 ];
 
+// Make two of every card
+let deck = [...cardTypes, ...cardTypes]
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+//Shuffle the deck
+deck = shuffle(deck);
+
+// Randomly populate the cards
+for (let i = 0; i < deck.length; i++ ) {
+  let cards = document.querySelectorAll('.card');
+  for (let animal in deck) {
+    cards[i].innerHTML = `<i class="fas ${deck[i]}"></i>`;
+  }
+}
+
+
+/*
+ * FUNCTIONALITY
+ */
+
 const cards = document.querySelectorAll('.card');
 
-let turn = 1;
+let turnsLeft = 2;
 let moves = 0;
 
-/*
- * DECLARE FUNCTIONS
- */
+let cardOne = null;
+let cardTwo = null;
+
+let result;
+
+// DO THE THING
+console.log(typeof cards);
 
 
 
-/*
- * DOING STUFF
- */
-
-// For each card
 for (const card of cards) {
-  // Listen for a click
-  card.addEventListener('click', function() {
-    // If it's your first card
-    if (turn === 1) {
-      card.classList.add('open');
-      console.log(`Turns: ${turn}`)
-      turn += 1;;
-    }
-    // If it's your second card
-    else if (turn === 2) {
-      card.classList.add('open');
-      console.log(`Turns: ${turn}. Go again.`);
-      turn = 1;
-    };
-  });
+ card.addEventListener('click', function () {
+   if (cardOne === null /* && this not already open */ ) {
+
+     // Set the variable to the icon class
+     cardOne = card.querySelector('svg').getAttribute('data-icon');
+     console.log(`Card one = ${cardOne}`);
+   }
+   else if (cardTwo === null /* && this not already open */) {
+     // Set the variable to the icon class
+     cardTwo = card.querySelector('svg').getAttribute('data-icon');
+     console.log(`Card two = ${cardTwo}`);
+
+     //Compare cardOne and cardTwo
+     if (cardOne === cardTwo) {
+       result = true;
+     } else {
+       result = false;
+     }
+
+     console.log (result);
+     // Reset the variables
+     cardOne = null;
+     cardTwo = null;
+   }
+ });
 }
+
+
+//
+// /*
+//  * DECLARE FUNCTIONS
+//  */
+//
+// // Check if it's a match
+// function checkMatch() {
+//   console.log(`Check to see if it's a match`)
+//
+// };
+//
+// // Keep track of what turn and move you're on
+// function turnCount() {
+//   // If it's the first card,
+//   if (turnsLeft === 2) {
+//     turnsLeft -= 1;
+//     moves += 1;
+//   }
+//   // if it's the second card,
+//   else if (turnsLeft === 1) {
+//     turnsLeft = 2;
+//     moves += 1;
+//     console.log(`Try for your next match`)
+//   }
+// };
+//
+// // Turn cards over
+// function turnCard(e) {
+//   const thisCard = e.currentTarget;
+//   // If it's already open or matched, do nothing
+//   if (thisCard.classList.contains('open') || thisCard.classList.contains('matched')) {
+//     console.log(`Already open. You have ${turnsLeft} turns left.`);
+//   }
+//   // Otherwise...
+//   else {
+//     thisCard.classList.add('open');
+//     console.log(`Opened. You have ${turnsLeft} turns left.`);
+//     turnCount();
+//     checkMatch();
+//   }
+// ;}
+//
+//
+// /*
+//  * DOING STUFF
+//  */
+//
+//  for (const card of cards) {
+//    card.addEventListener('click', turnCard);
+//  }
