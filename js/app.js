5f05a4541d55f5f9497b8b80ca37a2f6a7714860
Dina Konto <dina.konto@gmail.com>
2020-03-17 18:18:55 +1100

    Tidied up file and cleared out old code

--- js/app.js
+++ js/app.js
@@ -1,6 +1,17 @@
+/*
+ *    STILL TO DO
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
+
+
 /* * * * * * * * *
- POPULATE THE HTML
+ GLOBAL VARIABLES
  * * * * * * * * */
+
+// Variable to access cards in the DOM
+const cards = document.querySelectorAll('.card')
 
 // List of card types
 const cardTypes = [
@@ -14,11 +25,17 @@ const cardTypes = [
   'fa-hippo'
 ];
 
-// Make two of every card
-let deck = [...cardTypes, ...cardTypes]
+// List of which cards are open
+let openCards = [];
+
+/* * * * * * * * *
+ FUNCTIONS
+ * * * * * * * * */
 
 // Shuffle the cards and display them randomly
 function resetDeck() {
+  // Make two of every card
+  let deck = [...cardTypes, ...cardTypes]
   // Shuffle function from http://stackoverflow.com/a/2450976
   function shuffle(array) {
       var currentIndex = array.length, temporaryValue, randomIndex;
@@ -33,10 +50,8 @@ function resetDeck() {
 
       return array;
   }
-
-  //Shuffle the deck
+  // Save the shuffled deck
   deck = shuffle(deck);
-
   // Randomly populate the cards
   for (let i = 0; i < deck.length; i++ ) {
     let cards = document.querySelectorAll('.card');
@@ -46,16 +61,7 @@ function resetDeck() {
   }
 }
 
-document.addEventListener('DOMContentLoaded', resetDeck());
-
-
-/* * * * * * * * *
- SHOW CARD SYMBOL ONCLICK
- * * * * * * * * */
-
-let openCards = [];
-
-
+// Show the card and add it to the openCards array
 function showCard(card) {
   card.classList.add('open');
   if (openCards.length < 2) {
@@ -66,35 +72,48 @@ function showCard(card) {
   }
 };
 
+// Check to see if two open cards match
 function checkMatch() {
-  console.log(`Checking for match on: ${openCards}.`);
-  let choices = document.querySelectorAll('.open');
-  console.log(choices);
-  // If it's a match
   if (openCards[0] == openCards[1]) {
-    choices[0].classList.add('match');
-    choices[1].classList.add('match');
-    choices[0].classList.remove('open');
-    choices[1].classList.remove('open');
-    openCards = [];
+    hasMatch();
   }
-  // If it's not
   else {
-    choices[0].classList.add('nomatch');
-    choices[1].classList.add('nomatch');
-    setTimeout(() => {
-      choices[0].classList.remove('nomatch');
-      choices[1].classList.remove('nomatch');
-      choices[0].classList.remove('open');
-      choices[1].classList.remove('open');
-    }, 1000);
-    openCards = [];
+    noMatch();
   }
 }
 
+// If they do match
+function hasMatch() {
+  let choices = document.querySelectorAll('.open');
+  choices[0].classList.add('match');
+  choices[1].classList.add('match');
+  choices[0].classList.remove('open');
+  choices[1].classList.remove('open');
+  openCards = [];
+}
 
-const cards = document.querySelectorAll('.card')
+// If they don't match
+function noMatch() {
+  let choices = document.querySelectorAll('.open');
+  choices[0].classList.add('nomatch');
+  choices[1].classList.add('nomatch');
+  setTimeout(() => {
+    choices[0].classList.remove('nomatch');
+    choices[1].classList.remove('nomatch');
+    choices[0].classList.remove('open');
+    choices[1].classList.remove('open');
+  }, 600);
+  openCards = [];
+}
 
+/* * * * * * * * *
+ EXECUTION
+ * * * * * * * * */
+
+// When DOM is ready, load randomised content
+document.addEventListener('DOMContentLoaded', resetDeck());
+
+// Listen for a click on each card
 for (const card of cards) {
   card.addEventListener('click', function () {
     if (openCards.length < 2 && !card.classList.contains('open')) {
@@ -102,156 +121,3 @@ for (const card of cards) {
     };
   });
 }
-
-
-// // Turn cards over
-// function turnCard(e) {
-//   const thisCard = e.currentTarget;
-//   // If it's already open or matched, do nothing
-//   if (thisCard.classList.contains('open') || thisCard.classList.contains('matched')) {
-//     console.log(`Already open. You have ${turnsLeft} turns left.`);
-//   }
-//   // Otherwise...
-//   else {
-//     thisCard.classList.add('open');
-//     console.log(`Opened. You have ${turnsLeft} turns left.`);
-//     turnCount();
-//     checkMatch();
-//   }
-// ;}
-
-
-
-
-
-
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// /*
-//  * FUNCTIONALITY
-//  */
-//
-// const cards = document.querySelectorAll('.card');
-//
-// let turnsLeft = 2;
-// let moves = 0;
-//
-// let cardOne = null;
-// let cardTwo = null;
-//
-// let result;
-//
-// // DO THE THING
-// console.log(typeof cards);
-//
-//
-//
-// for (const card of cards) {
-//  card.addEventListener('click', function () {
-//    if (cardOne === null /* && this not already open */ ) {
-//
-//      // Set the variable to the icon class
-//      cardOne = card.querySelector('svg').getAttribute('data-icon');
-//      console.log(`Card one = ${cardOne}`);
-//    }
-//    else if (cardTwo === null /* && this not already open */) {
-//      // Set the variable to the icon class
-//      cardTwo = card.querySelector('svg').getAttribute('data-icon');
-//      console.log(`Card two = ${cardTwo}`);
-//
-//      //Compare cardOne and cardTwo
-//      if (cardOne === cardTwo) {
-//        result = true;
-//      } else {
-//        result = false;
-//      }
-//
-//      console.log (result);
-//      // Reset the variables
-//      cardOne = null;
-//      cardTwo = null;
-//    }
-//  });
-// }
-//
-//
-// //
-// // /*
-// //  * DECLARE FUNCTIONS
-// //  */
-// //
-// // // Check if it's a match
-// // function checkMatch() {
-// //   console.log(`Check to see if it's a match`)
-// //
-// // };
-// //
-// // // Keep track of what turn and move you're on
-// // function turnCount() {
-// //   // If it's the first card,
-// //   if (turnsLeft === 2) {
-// //     turnsLeft -= 1;
-// //     moves += 1;
-// //   }
-// //   // if it's the second card,
-// //   else if (turnsLeft === 1) {
-// //     turnsLeft = 2;
-// //     moves += 1;
-// //     console.log(`Try for your next match`)
-// //   }
-// // };
-// //
-// // // Turn cards over
-// // function turnCard(e) {
-// //   const thisCard = e.currentTarget;
-// //   // If it's already open or matched, do nothing
-// //   if (thisCard.classList.contains('open') || thisCard.classList.contains('matched')) {
-// //     console.log(`Already open. You have ${turnsLeft} turns left.`);
-// //   }
-// //   // Otherwise...
-// //   else {
-// //     thisCard.classList.add('open');
-// //     console.log(`Opened. You have ${turnsLeft} turns left.`);
-// //     turnCount();
-// //     checkMatch();
-// //   }
-// // ;}
-// //
-// //
-// // /*
-// //  * DOING STUFF
-// //  */
-// //
-// //  for (const card of cards) {
-// //    card.addEventListener('click', turnCard);
-// //  }
